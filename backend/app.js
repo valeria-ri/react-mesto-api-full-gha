1d26bb8a7708636d63a52cbf8c6eb0c70112f96c
Valeria Rybakova <valerian.rybakova@yandex.ru>
2023-07-12 03:51:31 +0300

    добавить краш-тест

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,12 @@ app.use(express.json());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(router);
 
 app.use(errorLogger);
